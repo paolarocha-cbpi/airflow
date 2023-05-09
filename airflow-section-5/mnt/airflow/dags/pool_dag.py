50bc43ef6342d22825d231d566e891bdc54070e3
Paola Rocha <lrocha@cbpi.com.mx>
2023-05-08 19:31:11 -0600

    airflow: Celery executor

--- airflow-section-5/mnt/airflow/dags/pool_dag.py
+++ airflow-section-5/mnt/airflow/dags/pool_dag.py
@@ -16,29 +16,35 @@ with DAG(dag_id='pool_dag', schedule_interval='0 0 * * *', default_args=default_
     get_forex_rate_EUR = SimpleHttpOperator(
         task_id='get_forex_rate_EUR',
         method='GET',
+        priority_weight=1,
+        pool='forex_api_pool',
         http_conn_id='forex_api',
         endpoint='/latest?base=EUR',
         xcom_push=True
     )
- 
+
     # get forex rates of JPY and push them into XCOM
     get_forex_rate_USD = SimpleHttpOperator(
         task_id='get_forex_rate_USD',
         method='GET',
+        priority_weight=2,
+        pool='forex_api_pool',
         http_conn_id='forex_api',
         endpoint='/latest?base=USD',
         xcom_push=True
     )
- 
+
     # get forex rates of JPY and push them into XCOM
     get_forex_rate_JPY = SimpleHttpOperator(
         task_id='get_forex_rate_JPY',
         method='GET',
+        priority_weight=3,
+        pool='forex_api_pool',
         http_conn_id='forex_api',
         endpoint='/latest?base=JPY',
         xcom_push=True
     )
- 
+
     # Templated command with macros
     bash_command="""
         {% for task in dag.task_ids %}
